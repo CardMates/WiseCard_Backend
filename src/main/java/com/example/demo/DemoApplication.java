1fa6e70eb737c4baa95e5b973129ad613430861a
yeonsoo <dfh56541@gmail.com>
2025-10-04 19:25:30 +0900

    cicd: jwt secret 추가

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableJpaRepositories(basePackages = "com.example.demo")
+@EnableRedisRepositories(basePackages = "com.example.demo.cache")
 public class DemoApplication {
 
     public static void main(String[] args) {
