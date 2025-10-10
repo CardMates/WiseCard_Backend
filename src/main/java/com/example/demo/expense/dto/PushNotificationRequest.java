c9d5839e6251eaac911239ddf2bb53f805ab3182
yeonsoo <dfh56541@gmail.com>
2025-10-11 00:34:02 +0900

    feat: 푸시알림으로부터 소비 내역 저장 TODO 추가

--- src/main/java/com/example/demo/expense/dto/PushNotificationRequest.java
+++ src/main/java/com/example/demo/expense/dto/PushNotificationRequest.java
@@ -1,6 +1,7 @@
 package com.example.demo.expense.dto;
 
 public record PushNotificationRequest(
+        // TODO: 유저 식별 위해 accessToken 추가
     String packageName,
     Long postedAt,
     String text,
