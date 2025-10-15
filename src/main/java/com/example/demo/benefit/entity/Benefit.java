e5fe0f9a1310ec874471dfe8410e481962a1ad2c
yeonsoo <dfh56541@gmail.com>
2025-10-15 16:14:44 +0900

    feat: summary 필드 타입 TEXT 로 변경

--- src/main/java/com/example/demo/benefit/entity/Benefit.java
+++ src/main/java/com/example/demo/benefit/entity/Benefit.java
@@ -6,18 +6,7 @@ import java.util.Optional;
 
 import com.example.demo.card.entity.Card;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -54,6 +43,8 @@ public class Benefit {
     @OneToMany(mappedBy = "benefit", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<CashbackBenefit> cashbackBenefits = new ArrayList<>();
 
+    @Lob
+    @Column(columnDefinition = "TEXT")
     private String summary;
 
     @Builder
