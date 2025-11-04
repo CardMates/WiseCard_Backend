5b07aeb66a26c2f08360d05c0be8698af1619aef
sominyun <somin455@naver.com>
2025-11-04 13:14:12 +0900

    feat: 회원가입&로그인 login에 통합

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -5,7 +5,6 @@ import com.example.demo.auth.dto.RefreshTokenRequest;
 import com.example.demo.auth.dto.TokenResponse;
 import com.example.demo.auth.service.KakaoLoginService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,24 +14,10 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final KakaoLoginService kakaoLoginService;
 
-    @PostMapping("/signup")
-    public ResponseEntity<TokenResponse> signup(@RequestBody AccessTokenRequest request) {
-        try {
-            TokenResponse token = kakaoLoginService.signup(request);
-            return ResponseEntity.ok(token);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-    }
-
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> login(@RequestBody AccessTokenRequest request) {
-        try {
-            TokenResponse token = kakaoLoginService.login(request);
-            return ResponseEntity.ok(token);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+        TokenResponse token = kakaoLoginService.login(request);
+        return ResponseEntity.ok(token);
     }
 
     @PutMapping("/reissue")
